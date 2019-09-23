ee96846689a99283605db51553cec3c3ef381a4e
Iain Collins <me@iaincollins.com>
2019-09-23 01:58:48 +0100

    Improve structured data view

--- components/article-metadata/structured-data.js
+++ components/article-metadata/structured-data.js
@@ -60,6 +60,7 @@ class TestResult extends React.Component {
         <span className='structured-data__test-summary'>
           <span className='structured-data__test-icon'>{icon} </span>
           <span className='structured-data__test-description'>{description || test}</span><br/>
+          {value && String(value) !== '[object Object]' && <span className='structured-data__test-value'>└─ {String(value)}</span> }
         </span>
       </li>
     )
