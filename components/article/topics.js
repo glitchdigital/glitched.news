31fe93dfba17cd344a8cb3a807f955cd97de24ad
Iain Collins <me@iaincollins.com>
2019-09-28 20:07:03 +0100

    Fix bugs and improve labels on topic page

--- components/article/topics.js
+++ components/article/topics.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 export default class extends React.Component {
   render() {
@@ -11,19 +11,19 @@ export default class extends React.Component {
       <>
         <hr/>
         <h3>Topics &amp; keywords</h3>
-        <p className="lead">
+        <p className='lead'>
           Found <strong>{topics.keywords.length} keywords</strong> and identified <strong>{topics.topics.length} topics</strong> that may be related to the article.
         </p>
         <h4>Keywords</h4>
-        {(!topics.keywords || topics.keywords.length === 0) && <p className="text-muted">No keywords found.</p>}
-        <ul className="list-unstyled">
+        {(!topics.keywords || topics.keywords.length === 0) && <p className='text-muted'>No keywords found.</p>}
+        <ul className='list-unstyled'>
         {topics.keywords.map((keyword, i) => (
           <li key={`keyword-${i}`}>
             <p>
-              <span className="badge badge-pill badge-primary">{keyword.name}</span> <strong>{keyword.count || 1}</strong> mentions of this keyword
+              <span className='lead'><span className='badge badge-pill badge-primary'>{keyword.name}</span></span> <strong>{keyword.count || 1}</strong> mentions of this keyword
               {keyword.url && (
                 <>
-                  {' '}<br/><small><a target="_blank" href={keyword.url} rel='noreferrer'>{keyword.name} on Wikipedia</a></small>
+                  {' '}<br/><small><a target='_blank' href={keyword.url} rel='noreferrer'>{keyword.name} on Wikipedia</a></small>
                 </>
               )}
             </p>
@@ -32,15 +32,15 @@ export default class extends React.Component {
         </ul>
         <hr/>
         <h4>Topics</h4>
-        {(!topics.keywords || topics.keywords.length === 0) && <p className="text-muted">No topics identified.</p>}
-        <ul className="list-unstyled">
+        {(!topics.topics || topics.topics.length === 0) && <p className='text-muted'>No topics identified.</p>}
+        <ul className='list-unstyled'>
         {topics.topics.map((topic, i) => (
           <li key={`topic-${i}`}>
             <p>
-              <span className="badge badge-pill badge-primary">{topic.name}</span> <strong>{topic.count || 1}</strong> possible references to this topic
+              <span className='lead'><span className='badge badge-pill badge-primary'>{topic.name}</span></span> <strong>{topic.count || 1}</strong> possible references to this topic
               {topic.url && (
                 <>
-                  {' '}<br/><small><a target="_blank" href={topic.url} rel='noreferrer'>{topic.name} on Wikipedia</a></small>
+                  {' '}<br/><small><a target='_blank' href={topic.url} rel='noreferrer'>{topic.name} on Wikipedia</a></small>
                 </>
               )}
             </p>
