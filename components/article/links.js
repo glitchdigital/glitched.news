2f3201046a10a1ecde3019cd7dfe96f89d54ef65
Iain Collins <iaincollins@economist.com>
2019-09-30 21:29:10 +0100

    Improve links and homepage views when no data

--- components/article/links.js
+++ components/article/links.js
@@ -24,33 +24,37 @@ export default class extends React.Component {
         <p className="lead">
           Found <strong>{links.links.length || 0}</strong> links on this page to <strong>{Object.keys(linksByDomain).length}</strong> domains.
         </p>
-        <h4>Links by domain</h4>
-        <ul>
-          {Object.keys(linksByDomain).map((domain, i) => (
-            <li key={`link-domain-${i}-${domain}`}>
-              <strong>{domain}</strong> ({linksByDomain[domain].length})
-            </li>
-          ))}
-        </ul>
-        <hr/>
-        {Object.keys(linksByDomain).map(domain => (
+        { links.links.length > 0 && 
           <>
-            <h5>{domain}</h5>
+            <h4>Links by domain</h4>
             <ul>
-            {linksByDomain[domain].map((link, i) => (
-              <li key={`link-url-${i}-${link.url}`}>
-                <Link
-                  href={{
-                    pathname: '/article',
-                    query: { url: link.url }
-                  }}
-                  as={`/article?url=${link.url}`}
-                ><a rel='noreferrer'>{link.title || link.url}</a></Link>
-              </li>
-            ))}
+              {Object.keys(linksByDomain).map((domain, i) => (
+                <li key={`link-domain-${i}-${domain}`}>
+                  <strong>{domain}</strong> ({linksByDomain[domain].length})
+                </li>
+              ))}
             </ul>
+            <hr/>
+            {Object.keys(linksByDomain).map(domain => (
+              <>
+                <h5>{domain}</h5>
+                <ul>
+                {linksByDomain[domain].map((link, i) => (
+                  <li key={`link-url-${i}-${link.url}`}>
+                    <Link
+                      href={{
+                        pathname: '/article',
+                        query: { url: link.url }
+                      }}
+                      as={`/article?url=${link.url}`}
+                    ><a rel='noreferrer'>{link.title || link.url}</a></Link>
+                  </li>
+                ))}
+                </ul>
+              </>
+            ))}
           </>
-        ))}
+        }
       </>
     )
   }
--- components/homepage.js
+++ components/homepage.js
@@ -21,33 +21,37 @@ export default class extends React.Component {
         <p className='lead'>
           Found <strong>{homepage.links.length || 0}</strong> links on the homepage to <strong>{Object.keys(linksByDomain).length}</strong> domains.
         </p>
-        <h4>Links by domain</h4>
-        <ul>
-          {Object.keys(linksByDomain).map((domain, i) => (
-            <li key={`homepage-link-domain-${i}-${domain}`}>
-              <strong>{domain}</strong> ({linksByDomain[domain].length})
-            </li>
-          ))}
-        </ul>
-        <hr/>
-        {Object.keys(linksByDomain).map(domain => (
+        { homepage.links.length > 0 && 
           <>
-            <h5>{domain}</h5>
+            <h4>Links by domain</h4>
             <ul>
-            {linksByDomain[domain].map((link, i) => (
-              <li key={`homepage-link-url-${i}-${link.url}`}>
-                <Link
-                  href={{
-                    pathname: '/article',
-                    query: { url: link.url }
-                  }}
-                  as={`/article?url=${link.url}`}
-                ><a rel='noreferrer'>{link.title || link.url}</a></Link>
-              </li>
-            ))}
+              {Object.keys(linksByDomain).map((domain, i) => (
+                <li key={`homepage-link-domain-${i}-${domain}`}>
+                  <strong>{domain}</strong> ({linksByDomain[domain].length})
+                </li>
+              ))}
             </ul>
+            <hr/>
+            {Object.keys(linksByDomain).map(domain => (
+              <>
+                <h5>{domain}</h5>
+                <ul>
+                {linksByDomain[domain].map((link, i) => (
+                  <li key={`homepage-link-url-${i}-${link.url}`}>
+                    <Link
+                      href={{
+                        pathname: '/article',
+                        query: { url: link.url }
+                      }}
+                      as={`/article?url=${link.url}`}
+                    ><a rel='noreferrer'>{link.title || link.url}</a></Link>
+                  </li>
+                ))}
+                </ul>
+              </>
+            ))}
           </>
-        ))}
+        }
       </>
     )
   }
