b7b0436a6bf3f0b1d192b5c21543111b660cf760
Iain Collins <iaincollins@economist.com>
2019-09-30 21:23:51 +0100

    Improve structured data summary headlings

--- components/structured-data/errors-and-warnings.js
+++ components/structured-data/errors-and-warnings.js
@@ -38,7 +38,7 @@ export default ({testResults}) => (
 
       return (
         <>
-          <h5 className=''>{group}</h5>
+          <h5 className=''>{group.replace(/^Google > /, '').replace(/ > (.*)$/, '')}</h5>
           <ul className='list-unstyled mb-3 pl-3'>
             { testResults.groups[group].warnings.map(test => <TestResult group={group} key={JSON.stringify(test)} {...test} />) }
             { testResults.groups[group].failed.map(test => <TestResult group={group} key={JSON.stringify(test)} {...test} />) }
